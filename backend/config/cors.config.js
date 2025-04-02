3b0dd884c30d06e08e0d3e076165701cd1166296
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-04-03 00:15:48 +0530

    Update CORS configuration to change production origin

--- backend/config/cors.config.js
+++ backend/config/cors.config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["https://she-shield-roan.vercel.app", "http://localhost:5173"],
+  origin: ["https://she-shield-eight.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
